f250b1be525b53ef86b4781170b41c5a66807e3f
shouravrahman <shouravrahman006@gmail.com>
2023-11-18 01:09:59 +0600

    feat: resets and utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,37 @@ module.exports = {
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				body: ["Poppins", "sans-serif"], // Example font stack, replace with your fonts
+				custom: ["YourCustomFont", "sans-serif"], // Add your custom font stack here
+			},
+			fontSize: {
+				sm: "0.875rem",
+				base: "1rem",
+				lg: "1.125rem",
+				xl: "1.25rem",
+				"2xl": "1.5rem",
+				"3xl": "1.875rem",
+				"4xl": "2.25rem",
+				"5xl": "3rem",
+				"6xl": "4rem",
+			},
+			spacing: {
+				"1/2": "50%",
+				"1/3": "33.333333%",
+				"2/3": "66.666667%",
+				"1/4": "25%",
+				"2/4": "50%",
+				"3/4": "75%",
+			},
+			screens: {
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px",
+			},
+		},
 	},
 	plugins: [],
 };
