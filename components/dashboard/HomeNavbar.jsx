0d65590878f5e37e3812d891b538151b7c070fda
shouravrahman <shouravrahman006@gmail.com>
2023-11-20 21:27:33 +0600

    feat: dashboard home navigation

---
+++ components/dashboard/HomeNavbar.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Building2 } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const HomeNavbar = () => {
+	const pathname = usePathname();
+
+	const navlinks = [
+		{ title: "Dashboard", href: "/dashboard/home/overview" },
+		{ title: "Getting Started", href: "/dashboard/home/getting-started" },
+		{ title: "Recent Updates", href: "/dashboard/home/updates" },
+		{ title: "Announcements", href: "/dashboard/home/announcements" },
+	];
+	return (
+		<div className='h-32 p-5 header-bg border-b border-slate-300'>
+			<div className='flex space-x-3 ml-4'>
+				<div className='flex-center rounded-lg w-12 h-12 bg-white mr-4'>
+					<Building2 />
+				</div>
+				<div className='flex flex-col '>
+					<p className='font-semibold'>Hello, Shourav Rahman</p>
+					<span className='italic'>Sylhet, Bangladesh</span>
+				</div>
+			</div>
+
+			<nav className='sticky bottom-0 ml-4 mt-6 flex space-x-6 font-medium'>
+				{navlinks.map((item, idx) => {
+					return (
+						<Link
+							key={idx}
+							href={item.href}
+							className={`py-[1px]  mt-2  ${
+								pathname === item.href
+									? "border-b-4 border-black transition-all ease-in"
+									: ""
+							}`}
+						>
+							{item.title}
+						</Link>
+					);
+				})}
+			</nav>
+		</div>
+	);
+};
+
+export default HomeNavbar;
