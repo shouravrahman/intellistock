8912a7494425e030864f175559f9bfc17f027271
shouravrahman <shouravrahman006@gmail.com>
2023-11-21 23:10:39 +0600

    chore: active state

--- components/dashboard/sidebar/SubmenuItem.jsx
+++ components/dashboard/sidebar/SubmenuItem.jsx
@@ -1,25 +1,38 @@
+"use client";
 import clsx from "clsx";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Icon from "./Icon";
 
-const SubMenuItem = ({ submenuItem }) => (
-	<li
-		className={clsx(
-			"text-white",
-			"text-sm",
-			"flex",
-			"items-center",
-			"justify-between",
-			"cursor-pointer",
-			"p-3",
-			"px-20",
-			"hover:bg-purple-400",
-			"rounded-md"
-		)}
-	>
-		{" "}
-		<span>{submenuItem.icon}</span>
-		<Link href={submenuItem.href}>{submenuItem.title}</Link>
-	</li>
-);
+const SubMenuItem = ({ submenuItem }) => {
+	const pathname = usePathname();
+
+	return (
+		<li
+			className={clsx(
+				"text-white",
+
+				"flex-between",
+				"ml-4",
+				"my-2",
+				"gap-x-8",
+				"cursor-pointer",
+				"p-2",
+				// "ml-10",
+				pathname === submenuItem.href
+					? "bg-[hsla(0,0%,100%,.07)]"
+					: "bg-[hsla(0,0%,100%,.02)]",
+				"hover:bg-[hsla(0,0%,100%,.07)]",
+				"rounded-md"
+			)}
+		>
+			{" "}
+			<Icon icon={submenuItem.icon} />
+			<Link className='text-sm font-medium flex-1' href={submenuItem.href}>
+				{submenuItem.title}
+			</Link>
+		</li>
+	);
+};
 
 export default SubMenuItem;
