b268ae1ad79a1d07d2de3e1f8cc3cdccc99042d7
shouravrahman <shouravrahman006@gmail.com>
2023-11-21 23:13:37 +0600

    chore: inventory page

--- app/(back-office)/dashboard/inventory/page.jsx
+++ app/(back-office)/dashboard/inventory/page.jsx
@@ -1,7 +1,29 @@
+import FixedHeader from "@/components/dashboard/FixedHeader";
+import InventoryOptionCard from "@/components/dashboard/InventoryOptionCard";
+import { optionsData } from "@/constants/inventoryOptionsData";
+
 import React from "react";
 
 const Inventory = () => {
-	return <div>Inventory</div>;
+	return (
+		<div>
+			<FixedHeader href='/dashboard/inventory/items/new' />
+			<div className='grid grid-cols-1 lg:grid-cols-2 px-16 py-8 gap-6'>
+				{optionsData?.map((item, idx) => {
+					return (
+						<InventoryOptionCard
+							key={idx}
+							title={item.title}
+							description={item.description}
+							linkTitle={item.linkTitle}
+							href={item.href}
+							img={item.img}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default Inventory;
