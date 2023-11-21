51801503d00ac8803924da1855603b5e760fb7af
shouravrahman <shouravrahman006@gmail.com>
2023-11-21 23:10:06 +0600

    chore: data arrays for links

---
+++ constants/inventoryOptionsData.js
@@ -0,0 +1,34 @@
+export const optionsData = [
+	{
+		title: "Items",
+		description: "Create standalone items and services that you buy and sell",
+		linkTitle: "New Item",
+		href: "/new",
+		enabled: false,
+		img: "/tshirt.svg",
+	},
+	{
+		title: "Item Groups",
+		description: "Create multiple variants of the same item using Item Groups",
+		linkTitle: "New Item Group",
+		href: "/new",
+		enabled: true,
+		img: "/group.svg",
+	},
+	{
+		title: "Composite Items",
+		description: "Bundle differnet items together and sell them as kits",
+		linkTitle: "New composite item",
+		href: "/new",
+		enabled: false,
+		img: "/wardrobe.svg",
+	},
+	{
+		title: "Price Lists",
+		description: "Tweak your item prices for specific contacts or transcations",
+		linkTitle: "New Item Group",
+		href: "/new",
+		enabled: false,
+		img: "/list.svg",
+	},
+];
