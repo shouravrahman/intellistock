767a2f624292b7b0012d0f6813d6e8c281989346
shouravrahman <shouravrahman006@gmail.com>
2023-11-24 04:09:39 +0600

    chore: react toastify imports

--- app/(back-office)/dashboard/layout.jsx
+++ app/(back-office)/dashboard/layout.jsx
@@ -3,6 +3,8 @@ import Header from "@/components/dashboard/Header";
 import Sidebar from "@/components/dashboard/sidebar";
 import useSidebarState from "@/components/dashboard/sidebar/state/sidebarState";
 import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = ({ children }) => {
 	const { open } = useSidebarState();
@@ -11,12 +13,25 @@ const Layout = ({ children }) => {
 			<div className='w-full flex-1 min-h-screen'>
 				<Sidebar />
 			</div>
+			<ToastContainer
+				position='bottom-center'
+				autoClose={5000}
+				hideProgressBar
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss={false}
+				draggable
+				pauseOnHover
+				theme='dark'
+			/>
 			<main
 				className={`w-full ${
 					open ? "ml-60" : "ml-20"
 				} flex-2 bg-transparent transition-all ease-in-out duration-600 ml-1`}
 			>
 				<Header />
+
 				{children}
 			</main>
 		</div>
