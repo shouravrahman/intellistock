78e48dcad2bac5195609c601c0f14260148f0f2e
shouravrahman <shouravrahman006@gmail.com>
2023-11-25 23:26:19 +0600

    fix: replaced loading with submitting state

--- components/FormInputs/SubmitButton.jsx
+++ components/FormInputs/SubmitButton.jsx
@@ -1,17 +1,16 @@
 import { Loader2, Plus } from "lucide-react";
-import React from "react";
 
-const SubmitButton = ({ isLoading, title }) => {
+const SubmitButton = ({ isSubmitting, title }) => {
 	return (
 		<div className='sm:col-span-1 mt-6'>
 			<button
 				className='text-vercel-white bg-vercel-black font-medium rounded-lg text-base py-3 px-3 text-center hover:bg-[#222]'
 				type='submit'
 			>
-				{isLoading ? (
+				{isSubmitting ? (
 					<Loader2 className='text-vercel-white h-6 w-6' />
 				) : (
-					<div className=' font-medium flex-between'>
+					<div className='font-medium flex items-center'>
 						<Plus className='h-6 w-6 mr-2' />
 						<p>Save {title}</p>
 					</div>
