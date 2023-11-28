a6128f9725f89d8a1c07f8ada9b4665d3b7b11aa
shouravrahman <shouravrahman006@gmail.com>
2023-11-28 23:58:21 +0600

    refactor: remove unused imports

--- app/(back-office)/dashboard/home/layout.jsx
+++ app/(back-office)/dashboard/home/layout.jsx
@@ -1,9 +1,4 @@
-import Header from "@/components/dashboard/Header";
 import HomeNavbar from "@/components/dashboard/HomeNavbar";
-import Sidebar from "@/components/dashboard/sidebar";
-import useSidebarState from "@/components/dashboard/sidebar/state/sidebarState";
-import React from "react";
-import Poppins from "next/font/google";
 const Layout = ({ children }) => {
 	return (
 		<main className='font-noto font-medium h-screen bg-teal-100'>
