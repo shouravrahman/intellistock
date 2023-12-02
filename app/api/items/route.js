d2ce9050023f5feb79fbeca019aecc031de4762d
shouravrahman <shouravrahman006@gmail.com>
2023-12-02 23:42:21 +0600

    feat: get and post route

--- app/api/items/route.js
+++ app/api/items/route.js
@@ -4,31 +4,47 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
 	try {
-		// const {
-		// 	title,
-		// 	description,
-		// 	category,
-		// 	sku,
-		// 	barcode,
-		// 	quantity,
-		// 	unit,
-		// 	brand,
-		// 	buyingPrice,
-		// 	sellingPrice,
-		// 	suppliers,
-		// 	reorderPoint,
-		// 	warehouse,
-		// 	imageUrl,
-		// 	weight,
-		// 	dimensions,
-		// 	taxRate,
-		// 	notes,
-		// } = await req.json();
-		const itemData = await req.json();
+		const {
+			title,
+			description,
+			category,
+			sku,
+			barcode,
+			quantity,
+			unit,
+			brand,
+			buyingPrice,
+			sellingPrice,
+			suppliers,
+			reorderPoint,
+			warehouse,
+			imageUrl,
+			weight,
+			dimensions,
+			taxRate,
+			notes,
+		} = await req.json();
 
 		const item = await db.item.create({
 			data: {
-				...itemData,
+				title,
+				description,
+				category: { connect: { id: category } },
+				sku,
+				barcode,
+				quantity,
+				unit: { connect: { id: unit } },
+				brand: { connect: { id: brand } },
+				buyingPrice,
+				sellingPrice,
+				suppliers: { connect: { id: suppliers } },
+				reorderPoint,
+				warehouse: { connect: { id: warehouse } },
+				imageUrl,
+				weight,
+				dimensions,
+				taxRate,
+				notes,
 			},
 		});
 
@@ -39,6 +55,22 @@ export async function POST(req) {
 			{
 				error,
 				message: "Failed to create a new item",
+			},
+			{ status: 500 }
+		);
+	}
+}
+export async function GET() {
+	try {
+		const items = await db.item.findMany();
+
+		return NextResponse.json(items);
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json(
+			{
+				error,
+				message: "Failed to fetch Items",
 			},
 			{ status: 500 }
 		);
