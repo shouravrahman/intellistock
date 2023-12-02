0323d597681ede424cf2e3f6828ac5b0ac2dd495
shouravrahman <shouravrahman006@gmail.com>
2023-12-03 00:37:16 +0600

    hardcoded id removed

--- validations/add-stock-schema.js
+++ validations/add-stock-schema.js
@@ -4,32 +4,6 @@ export const addStockSchema = z.object({
 	addedStockQuantity: z.coerce.number().min(2),
 	referenceNumber: z.string().min(5),
 	notes: z.string().max(255),
-	receivingWarehouseId: z
-		.string()
-		.refine(
-			(data) =>
-				[
-					"65623a092ee79219ea3a4f61",
-					"65623a092ee79219ea3a4f63",
-					"65623a092ee79219ea3a4f62",
-					"65623a092ee79219ea3a4f64",
-				].includes(data),
-			{
-				message: "Invalid Warehouse Type",
-			}
-		),
-	itemId: z
-		.string()
-		.refine(
-			(data) =>
-				[
-					"65623a092ee79219ea3a4f61",
-					"65623a092ee79219ea3a4f63",
-					"65623a092ee79219ea3a4f62",
-					"65623a092ee79219ea3a4f64",
-				].includes(data),
-			{
-				message: "Invalid Warehouse Type",
-			}
-		),
+	receivingWarehouseId: z.string(),
+	itemId: z.string(),
 });
