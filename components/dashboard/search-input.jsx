f5623a63291e1d648981cab2950736a14716f5ea
shouravrahman <shouravrahman006@gmail.com>
2024-01-05 19:04:55 +0600

    styles fix: margin

--- components/dashboard/search-input.jsx
+++ components/dashboard/search-input.jsx
@@ -6,17 +6,16 @@ const SearchInput = ({ className }) => {
 	return (
 		<div className='relative w-full '>
 			<div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
-				<Search className='w-4 h-4 hidden md:block' />
+				<Search className='w-4 h-4 hidden mr-4 md:block' />
 			</div>
 			<input
 				type={"search"}
 				id='simple-search'
 				className={cn(
-					"bg-form  text-foreground text-sm rounded-lg focus:ring-ring focus:border-ring block w-full pl-10 py-1 px-2 outline-none",
+					"bg-form  text-foreground text-sm rounded-lg focus:ring-ring focus:border-ring block w-full pl-10 py-1  outline-none",
 					className
 				)}
 				placeholder='Search in customers...'
-				required
 			/>
 		</div>
 	);
